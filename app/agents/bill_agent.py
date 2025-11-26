8c11c5ad930c7cdacf25e5b5e8da11eb1d9ef478
this-is-sreehari <133988894+this-is-sreehari@users.noreply.github.com>
2025-11-27 00:47:44 +0530

    refactor: modify prompt and final data returned

--- app/agents/bill_agent.py
+++ app/agents/bill_agent.py
@@ -1,4 +1,5 @@
 from app.services.gemini_client import GeminiClient
+from app.utils.json_utils import clean_json
 
 
 class BillAgent:
@@ -17,10 +18,19 @@ class BillAgent:
             "hospital_name": "",
             "bill_amount": "",
             "bill_date": "",
-            "line_items": []
+            "line_items": [
+                {{
+                    "description": "",
+                    "quantity": "",
+                    "price": ""
+                }}
+            ]
         }}
 
         TEXT:
         {text}
         """
-        return await self.llm.generate(prompt)
+        result = await self.llm.generate(prompt)
+        cleaned_result = clean_json(result)
+
+        return cleaned_result
