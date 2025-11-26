353084adf1c3fc6254be829d2c696c34ccdc6c8f
this-is-sreehari <133988894+this-is-sreehari@users.noreply.github.com>
2025-11-27 00:50:14 +0530

    refactor: modify the final data returned by discharge agent

--- app/agents/discharge_agent.py
+++ app/agents/discharge_agent.py
@@ -1,4 +1,5 @@
 from app.services.gemini_client import GeminiClient
+from app.utils.json_utils import clean_json
 
 
 class DischargeAgent:
@@ -26,4 +27,7 @@ class DischargeAgent:
             "summary": ""
         }}
         """
-        return await self.llm.generate(prompt)
+        result = await self.llm.generate(prompt)
+        cleaned_result = clean_json(result)
+
+        return cleaned_result
